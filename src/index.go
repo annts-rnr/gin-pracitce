9a79b30b01f4d0197f9d9345dc845d017bb485db
annts095 <anan095.k@gmail.com>
2021-10-25 00:44:01 +0900

    feat /htmlで静的HTMLファイルを読み込み

--- src/index.go
+++ src/index.go
@@ -14,5 +14,8 @@ func main() {
 			"title": "Main website",
 		})
 	})
+	r.GET("html", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
