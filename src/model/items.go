8459357cdc7f2e5a0f69dd59029acf68ec116dfb
anan-tasuku <anantasuku@team-lab.com>
2022-01-09 17:29:04 +0900

    fix 価格をint型に修正

--- src/model/items.go
+++ src/model/items.go
@@ -4,5 +4,5 @@ type Item struct {
 	ID       int    `gorm:"primary_key;not null"`
 	Title    string `gorm:"type:varchar(200);not null"`
 	Contents string `gorm:"type:varchar(400)"`
-	Price    string `gorm:"type:integer"`
+	Price    int    `gorm:"type:integer"`
 }
